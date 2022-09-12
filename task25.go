55bb54ed2415ea67f4a7e41dbebf2738b31c9a45
Вадим Чельник <vadimcelnik@Air-Vadim.Dlink>
2022-09-12 21:16:51 +0300

    25

---
+++ task25.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func mySleep(duration time.Duration) {
+	var timer = time.After(duration)
+	<-timer
+}
+
+func main() {
+	//time.Sleep(-1)
+	mySleep(10000000000 / 5)
+	fmt.Println("go")
+}
