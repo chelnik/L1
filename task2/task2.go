3f971c678705b596dee384b3e8c1cf9569db7f57
Vadim Chelnik <sindbadseafarer@yandex.ru>
2022-09-28 10:45:12 +0400

    refactor

--- task2/task2.go
+++ task2/task2.go
@@ -5,25 +5,24 @@ import (
 )
 
 func main() {
-	//t := time.Now()
 	message := make(chan int)
 	array := [5]int{2, 4, 6, 8, 10}
 	go func() {
 		for _, number := range array {
 			message <- number * number
-			//time.Sleep(time.Millisecond * 500)
 		}
 		close(message)
 	}()
 	for msg := range message {
 		fmt.Println(msg)
 	}
-	//for {
-	//	msg, open := <-message
-	//	if !open {
-	//		break
-	//	}
-	//	fmt.Println(msg)
-	//}
-	//fmt.Println(time.Since(t).Nanoseconds())
 }
+
+// более многословный вариант чтения из канала
+//for {
+//	msg, open := <-message
+//	if !open {
+//		break
+//	}
+//	fmt.Println(msg)
+//}
--- task3/task3.go
+++ task3/task3.go
@@ -5,14 +5,12 @@ import (
 )
 
 func main() {
-	//t := time.Now()
 	var sum int
 	message := make(chan int)
 	array := [5]int{2, 4, 6, 8, 10}
 	go func() {
 		for _, number := range array {
 			message <- number * number
-			//time.Sleep(time.Millisecond * 500)
 		}
 		close(message)
 	}()
@@ -21,5 +19,4 @@ func main() {
 		sum += msg
 	}
 	fmt.Println(sum)
-	//fmt.Println(time.Since(t).Nanoseconds())
 }
